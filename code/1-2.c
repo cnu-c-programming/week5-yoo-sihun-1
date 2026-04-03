62340c11b6cf3c3af5ea1b8a1476b7a25b5d698f
yosi475 <yosi475@naver.com>
2026-04-03 10:22:24 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum_range(int *start, int *end) {
+    int total = 0;
+    while (start < end) {
+        total += *start; 
+        start++;         
+    }
+    return total;
+}
+
+int main() {
+    int data[] = {10, 20, 30, 40, 50, 60 ,70};
+
+    int result = sum_range(&data[1], &data[4]);
+
+    printf("%d\n", result);
+    return 0;
+}
