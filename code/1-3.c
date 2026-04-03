fb08662342744508638f36573778ec3bff458de1
yosi475 <yosi475@naver.com>
2026-04-03 10:22:38 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print_reverse(int *arr, int size) {
+    arr = arr + (size - 1);
+    for (int i = 0; i < size; i++) {
+        printf("%d", *arr); 
+        arr--;  
+    }
+}
+
+
+int main() {
+    int nums[] = {1, 2, 3, 4, 5};
+
+    print_reverse(nums, 5);
+    printf("\n");
+
+    return 0;
+}
